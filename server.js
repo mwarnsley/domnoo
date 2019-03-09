d5876f27775f174f56b9c6f874676b5982a68f03
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-03-09 17:07:13 -0500

    Fixing the server file

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-
 const app = express();
-
-app.use(serveStatic(path.join(__dirname, 'build')));
-
 const PORT = process.env.PORT || 4444;
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on Port: ${PORT}`);
